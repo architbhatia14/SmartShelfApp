37cd32e5cce6b6ce2b074fe8150d723d25aa5c11
Archit Bhatia <architbhatia@cmail.carleton.ca>
2019-10-09 17:43:04 -0400

    lab5 third commit

--- Lab-5/lab5/app/src/main/java/com/example/lab5/MainActivity.kt
+++ Lab-5/lab5/app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.lab5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+
 }
+
